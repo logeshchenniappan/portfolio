e4ee4a2d3c3b27abb05b0353ee3df5ef57450072
Logesh C <91900910+logeshchenniappan@users.noreply.github.com>
2025-06-09 22:21:48 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7167d2cf133a4875b7e23caf545ee402',
+  rootNotionPageId: '20d974ade26e80e78945ff0f130b78ab',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -53,16 +53,16 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [   
     {
-      title: 'Blog',
-      pageId: '588031c1e65d4dfa9d58e2492c9c1b7e'
+      title: 'About',
+      pageId: '20d974ade26e81fe8371e9ab958af046'
     },    
     {
-      title: 'About',
-      pageId: 'cf2ae3c4839e4e09a1b4a33cc600c409'
+      title: 'Projects',
+      pageId: '20d974ade26e80c5ba3aca20d59d9927'
     },
     {
-      title: 'Contact',
-      pageId: '5f7e562fef814299b10c68c52e920c9a'
+      title: 'Blog',
+      pageId: '20d974ade26e812a9965fa9f15af9a48'
     }
   ]
 })
