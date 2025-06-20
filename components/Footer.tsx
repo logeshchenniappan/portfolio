441eef08ea46eb66454a17603667ea4539b8ac9b
Logesh C <91900910+logeshchenniappan@users.noreply.github.com>
2025-06-20 18:19:02 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}><a href="https://wr8.in">wr8</a> by <a href="https://twitter.com/jotzilla">{config.author}</a></div>
+      <div className={styles.copyright}><a href="https://logeshc.com">Designed</a> by <a href="https://twitter.com/logesh__c">{config.author}</a></div>
 
       <div className={styles.settings}>
         {hasMounted && (
